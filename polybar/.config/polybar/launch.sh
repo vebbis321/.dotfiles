01f9e75c6df44ebfe9ab3aa3c9bd1b3016da7aec
thel <57636432+vebbis321@users.noreply.github.com>
2026-01-19 13:09:21 +0100

    polybar env

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,7 +2,16 @@
 
 killall -q polybar
 
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do
+  sleep 1
+done
+
+# Detect battery name
+if [ -d /sys/class/power_supply/BAT1 ]; then
+  export POLYBAR_BATTERY=BAT1
+else
+  export POLYBAR_BATTERY=BAT0
+fi
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload myBar &
