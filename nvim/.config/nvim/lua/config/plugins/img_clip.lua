e2cb8154687b43c86d0d53bb03f193ae7da50e3b
thel <57636432+vebbis321@users.noreply.github.com>
2026-04-22 19:15:08 +0200

    markdown yes

--- nvim/.config/nvim/lua/config/plugins/img_clip.lua
+++ nvim/.config/nvim/lua/config/plugins/img_clip.lua
@@ -4,13 +4,13 @@ return {
 	opts = {
 		default = {
 			file_name = "%y%m%d-%H%M%S",
-			template = "![Image](./$FILE_PATH)",
+			template = "![Image](./$FILE_PATH/$FILE_NAME)",
 			prompt_for_file_name = false,
 			process_cmd = "convert - -set gamma 0.4545 -",
 		},
 	},
 	keys = {
 		-- suggested keymap
-		{ "<C-p>", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+		{ "<leader-p>", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
 	},
 }
--- nvim/.config/nvim/lua/config/plugins/vimwiki.lua
+++ nvim/.config/nvim/lua/config/plugins/vimwiki.lua
@@ -1,39 +1,14 @@
 return {
-	"vimwiki/vimwiki",
+	"lervag/wiki.vim",
+	dependencies = {
+		"bullets-vim/bullets.vim",
+	},
 	init = function()
-		-- params wiki
-		local wiki_default = {
-			syntax = "markdown",
-			ext = ".md",
-			nested_syntaxes = {
-				["c"] = "c",
-				["rust"] = "rust",
-				["python"] = "python",
-				["lua"] = "lua",
-				["js"] = "js",
-				["bash"] = "bash",
-				["c++"] = "cpp",
-				["latex"] = "tex",
-			},
-		}
-
-		-- notes
-		local main_wiki = vim.deepcopy(wiki_default)
-		main_wiki.name = "wiki"
-		main_wiki.path = "~/wiki/"
-
-		vim.g.vimwiki_list = { main_wiki }
-		vim.g.vimwiki_ext2syntax = {
-			[".md"] = "markdown",
-			[".markdown"] = "markdown",
-			[".mdown"] = "markdown",
-		}
-		vim.g.vimwiki_filetypes = { "markdown" }
-		vim.g.vimwiki_hl_headers = 1 -- use alternating colours for different heading levels
-		vim.g.vimwiki_markdown_link_ext = 1 -- add markdown file extension when generating links
+		vim.g.name = "wiki"
+		vim.g.wiki_root = "~/wiki"
 
 		local keymap = vim.keymap
-		keymap.set("n", "<C-Space>", "<Plug>VimwikiToggleListItem<CR>0")
+		keymap.set("n", "<C-Space>", "<Plug>(bullets-toggle-checkbox)<CR>0")
 
 		local templates = require("config.modules.markdown_templates")
 
