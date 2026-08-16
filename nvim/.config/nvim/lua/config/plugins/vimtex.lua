5e0400e193a081312038fff98697088aa14117a0
thel <57636432+vebbis321@users.noreply.github.com>
2026-08-16 17:13:47 +0200

    more latex

--- nvim/.config/nvim/lua/config/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/config/plugins/vimtex.lua
@@ -6,6 +6,5 @@ return {
 		-- VimTeX configuration goes here, e.g.
 		vim.g.vimtex_view_method = "zathura"
 		vim.g.vimtex_compiler_method = "latexmk"
-		vim.g.vimtex_complete_enabled = 0 -- texlab
 	end,
 }
